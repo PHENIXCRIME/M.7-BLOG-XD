f4dff3a53755b6b403ee21df35e991cf91b401db
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-08 16:08:06 +0700

    CREATE ,  EDIT Story

---
+++ src/pages/Story.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function Story () {
+  const { storyId } = useParams()
+  return (
+    <div>
+      Story page {storyId}
+    </div>
+  )
+}
+
+export default Story
