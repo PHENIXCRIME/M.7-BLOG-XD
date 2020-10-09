68ad2a9daac9e2a1669febf8720ed42ade776ab2
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-09 16:09:31 +0700

    CREATE  , EDIT Typography

---
+++ src/components/Typography.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Text = styled.span`
+font-size : 5rem;
+text-shadow: 1px 1px #000;
+color :#fff;
+margin : 0.5rem ;
+`
+export const Title = styled(Text)`
+font-size : 6rem;
+`
+export const Subtitle = styled(Text)`
+font-size: 3rem ;
+font-style: italic;
+`
