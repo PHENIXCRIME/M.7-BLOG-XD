c8fb2ee5b99a00e6ad25cd1292e02043043b5362
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-09 16:10:58 +0700

    CREATE , SET Scroll

---
+++ src/utils/useScroll.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+function useScroll ({ scrollRange = 20 } = {}) {
+  const [isScroll, setScroll] = useState(false)
+
+  function handleScrollEvent () {
+    if (window.scrollY > scrollRange) return setScroll(true)
+
+    return setScroll(false)
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScrollEvent)
+
+    return () => document.removeEventListener('scroll')
+  }, [])
+
+  return isScroll
+}
+export default useScroll
