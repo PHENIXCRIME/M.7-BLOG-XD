404f2db78cd69ed14f37129faf2d94161d299703
PHENIXCRIME <photboatxeemus@icloud.com>
2020-10-09 16:07:39 +0700

    EDIT reset css , helmet , global styles , navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Reset } from 'styled-reset'
 import routes from './config/routes'
+import withHelmet from './utils/withHelmet'
+import GlobalStyles from './components/GlobalStyle'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
-    <Router>
-      <Suspense fallback='...loading'>
-        <Switch>
-          {Object.keys(routes).map(routeKey => (
-            <Route key={routeKey} {...routes[routeKey]} />
-          ))}
-        </Switch>
-      </Suspense>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Reset />
+      <Router>
+        <Navbar />
+        <Suspense fallback='...loading'>
+          <Switch>
+            {Object.keys(routes).map(routeKey => (
+              <Route key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   )
 }
 
-export default App
+export default withHelmet('Phenixcrime | Blog-App')(App)
